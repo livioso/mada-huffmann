76740dc4dc42149d39110651b157263953ae3a7d
Livio Bieri <livio@livio.li>
2014-05-05 16:46:58 +0200

    Ongoing work ;-)

--- huffmann/src/huffmann/Huffmann.java
+++ huffmann/src/huffmann/Huffmann.java
@@ -2,32 +2,60 @@ package huffmann;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class Huffmann {
-	/** A map where the key is a character contained in the
-	 *  file that is encoded using the Huffmann encoding */
-	private Map<Integer, Integer> mHuffmannMapping = new HashMap<>();
+	/** Map that holds the frequency for each character  */
+	private Map<Integer, Integer> characterFrequencyMap = new HashMap<>();
 	
-	/** This method is responsible for the encoding
-	 *  @param pathOfFileToEncode This is the file path that should be encoded.
-	 *  @param pathOfFileResult This is the file path of the resulting file. 
-	 *  @throws IOException */
-	public void encoding (String pathOfFileToEncode, String pathOfFileResult) throws IOException {
+	/** */
+	public void createFrequencyMap (String pathOfFileToEncode) throws IOException {
 		FileInputStream fileInput = new FileInputStream(pathOfFileToEncode);
 		
 		Integer character = 0;
 		while((character = fileInput.read()) != -1) {
 			// if the character is not yet there add it with the initial 
 			// occurrence of 0 - otherwise just increment it by one
-			int countCharacter = mHuffmannMapping.containsKey(character) ? 
-								 mHuffmannMapping.get(character) : 0;
-			mHuffmannMapping.put(character, countCharacter + 1);
+			int countCharacter = characterFrequencyMap.containsKey(character) ? 
+								 characterFrequencyMap.get(character) : 0;
+			characterFrequencyMap.put(character, countCharacter + 1);
 		}
 		
 		fileInput.close();
+	}
+	
+	public void sortFrequenceMap () {
 		
-		System.out.println(mHuffmannMapping);
+		List<Map.Entry<Integer, Integer>> characterFrequencyMapAsList = 
+			new ArrayList<Map.Entry<Integer, Integer>>(characterFrequencyMap.entrySet());
+
+		// sort the map according the values
+		Collections.sort(characterFrequencyMapAsList,
+		new Comparator<Object>() {
+	    	@SuppressWarnings("unchecked")
+			public int compare(Object o1, Object o2) {
+	    		Map.Entry<Integer, Integer> e1 = (Map.Entry<Integer, Integer>) o1;
+	    		Map.Entry<Integer, Integer> e2 = (Map.Entry<Integer, Integer>) o2;
+	            return ((Comparable<Integer>) e1.getValue()).compareTo(e2.getValue());
+	         }
+		});
+		
+		// now empty the unsorted map
+		characterFrequencyMap.clear();
+		
+		// ... and fill it with the sorted list
+		for (Map.Entry<Integer, Integer> each : characterFrequencyMapAsList) {
+			characterFrequencyMap.put(each.getKey(), each.getValue());
+		}
+	}
+
+	public void createHuffmanEncoding() {
+		// get possible root
+		characterFrequencyMap.get(0);
 	}
 }
--- huffmann/src/huffmann/HuffmannCLI.java
+++ huffmann/src/huffmann/HuffmannCLI.java
@@ -6,7 +6,9 @@ public class HuffmannCLI {
 	public static void main(String[] args) {
 		Huffmann huffmann = new Huffmann();
 		try {
-			huffmann.encoding("/Users/livio/Desktop/input.txt", "");
+			huffmann.createFrequencyMap("/Users/livio/Desktop/input.txt");
+			huffmann.sortFrequenceMap();
+			huffmann.createHuffmanEncoding();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
